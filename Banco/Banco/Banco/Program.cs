70f7b54fad8f49becbf327c9d8e21cb74656d5c6
Davi Antonio Ramos Araujo <124791732+daviramos1c@users.noreply.github.com>
2024-03-14 12:40:16 -0300

    Add files via upload

---
+++ Banco/Banco/Banco/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Banco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta = new Conta();
+
+            conta.mudarlimite(700);
+            conta.depositar(500); 
+            Console.WriteLine("Seu saldo é de R$" + conta.versaldo() + " e seu limite é de R$" + conta.verlimite()); //inicio
+
+            conta.depositar(500); //valor inicial
+            Console.WriteLine("você fez um depósito e está com o saldo de R$" + conta.versaldo()); //mostra saldo
+
+            conta.sacar(200); //saque
+            Console.WriteLine("você fez um saque e está com o saldo de R$" + conta.versaldo()); //mostra saldo
+
+
+
+        }
+    }
+}
