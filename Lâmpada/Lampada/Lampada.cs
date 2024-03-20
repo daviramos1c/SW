c7828ea80728db92c80b4fbc2af0622383d0e7df
Davi Antonio Ramos Araujo <124791732+daviramos1c@users.noreply.github.com>
2024-03-20 20:53:21 -0300

    Add files via upload

---
+++ Lâmpada/Lampada/Lampada.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lampada
+{
+    public class Lampada
+    {
+        private bool Ligada { get; set; }
+        private string? Cor { get; set; }
+        private int Potencia { get; set; }
+
+        public void Ligar()
+        {
+            Ligada = true;
+        }
+
+        public void Desligar()
+        {
+            Ligada = false;
+        }
+
+        public bool Estaligada()
+        { 
+            return Ligada; 
+        }
+
+        public void Verde()
+        {
+            Cor = "verde";
+        }
+
+        public void Azul()
+        {
+            Cor = "azul";
+        }
+
+        public string VerCor()
+        {
+            return Cor;
+        }
+
+        public void Pot50()
+        {
+            Potencia = 50;
+        }
+
+        public void Pot100()
+        {
+            Potencia = 100;
+        }
+
+        public int VerPot()
+        { 
+            return Potencia; 
+        }
+    }
+}
---
+++ Lâmpada/Lampada/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Lampada
+{
+    public class Programs
+    {
+        static void Main(string[] args)
+        {
+
+            Lampada lampada1 = new Lampada();
+
+            lampada1.Ligar();
+            Console.WriteLine("A lâmpada está ligada: " + lampada1.Estaligada());
+
+            lampada1.Desligar();
+            Console.WriteLine("A lâmpada está ligada: " + lampada1.Estaligada());
+
+            lampada1.Verde();
+            Console.WriteLine("A cor da lâmpada é: " + lampada1.VerCor());
+
+            lampada1.Azul();
+            Console.WriteLine("A cor da lâmpada é: " + lampada1.VerCor());
+
+            lampada1.Pot50();
+            Console.WriteLine("A potência da lâmpada é: " + lampada1.VerPot());
+
+            lampada1.Pot100();
+            Console.WriteLine("A potência da lâmpada é: " + lampada1.VerPot());
+
+
+        }
+    }
+}
