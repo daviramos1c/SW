f8aea041478336082f46586a60473c9edd5f3b7d
Davi Antonio Ramos Araujo <124791732+daviramos1c@users.noreply.github.com>
2024-04-12 21:13:59 -0300

    Add files via upload

---
+++ Pagamento/Pagamento/Pag.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pagamento
+{
+    class Pag
+    {
+
+        public string? Nome { get; set; }
+        public double Sal { get; set; }
+        public int Dependentes { get; set; }
+        public int Faltas { get; set; }
+        public int HorasExtrasDi { get; set; }
+        public int HorasExtrasNot { get; set; }
+        public double Descontos { get; set; }
+        public double Refeicoes { get; set; }
+        public double ValesR { get; set; }
+
+        private const double SalarioMinimo = 1000.00;
+        private const double Taxa = 0.08;
+        private const double RendaTaxa = 0.08;
+
+
+
+        public void Calculo()
+        {
+
+            double horasExtras = (HorasExtrasDi * Sal / 160) + (HorasExtrasNot * 1.2 * Sal / 160);
+            double salarioFamilia = Dependentes * 0.05 * SalarioMinimo;
+            double salarioBruto = Sal + horasExtras + salarioFamilia;
+            double inamps = Taxa * Sal;
+            double faltasDesconto = Faltas * Sal / 160;
+            double refeicoes = Refeicoes;
+            double vales = ValesR;
+            double descontoTotal = inamps + faltasDesconto + refeicoes + vales + Descontos + (RendaTaxa * salarioBruto);
+            double salarioLiquido = salarioBruto - descontoTotal;
+
+            Console.WriteLine($"Nome: " + Nome);
+            Console.WriteLine($"Salário: " + Sal);
+            Console.WriteLine($"Horas Extras: " + horasExtras);
+            Console.WriteLine($"Salário Família: " + salarioFamilia);
+            Console.WriteLine($"Salário Bruto: " + salarioBruto);
+            Console.WriteLine($"Desconto INAMPS: " + inamps);
+            Console.WriteLine($"Desconto por Faltas: " + faltasDesconto);
+            Console.WriteLine($"Desconto por Refeições: " + refeicoes);
+            Console.WriteLine($"Desconto por Vales: " + vales);
+            Console.WriteLine($"Descontos Eventuais: " + Descontos);
+            Console.WriteLine($"Imposto de Renda: " + RendaTaxa * salarioBruto);
+            Console.WriteLine($"Desconto Total: " + descontoTotal);
+            Console.WriteLine($"Salário Líquido: " + salarioLiquido);
+
+        }
+    }
+}
---
+++ Pagamento/Pagamento/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Pagamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pag pagamento = new Pag();
+
+
+            Console.WriteLine("Digite o nome:");
+            pagamento.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o salário:");
+            pagamento.Sal = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite as horas extras diurnas:");
+            pagamento.HorasExtrasDi = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite as horas extras noturnas:");
+            pagamento.HorasExtrasNot = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o número de dependentes:");
+            pagamento.Dependentes = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite a quantidade de faltas:");
+            pagamento.Faltas = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor de descontos eventuais:");
+            pagamento.Descontos = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor gasto com refeições:");
+            pagamento.Refeicoes = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor dos vales retirados:");
+            pagamento.ValesR = Convert.ToDouble(Console.ReadLine());
+
+            pagamento.Calculo();
+        }
+    }
+}
