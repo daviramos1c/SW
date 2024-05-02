42c25988ef78bf4803c8d4632968269914895dbb
Davi Antonio Ramos Araujo <124791732+daviramos1c@users.noreply.github.com>
2024-05-02 12:46:55 -0300

    Add files via upload

---
+++ Estoque/Estoque/Produto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estoque
+{
+    class Produto
+    {
+        private int codigo { get; set; }
+        private string? nome { get; set; }
+        private int estoque { get; set; }
+        private double valor { get; set; }
+
+        public Produto(int cod, int est, double val)
+        {
+            cod = codigo;
+            est = estoque;
+            val = valor;
+        }
+
+        public void entrada(int qtd)
+        {
+            estoque = estoque + qtd;
+            Console.WriteLine("Entrada de" + qtd);
+        }
+
+        public void venda(int qtd)
+        {
+            estoque = estoque - qtd;
+            Console.WriteLine("Venda de" + qtd);
+        }
+
+        public void imprimir()
+        {
+            Console.WriteLine("Dados do produto: ");
+            Console.WriteLine("Cod: " + codigo);
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Nome: " + estoque);
+            Console.WriteLine("Valor: " + valor);
+        }
+    }
+}
---
+++ Estoque/Estoque/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Estoque
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto(24, 0, 70);
+            p.imprimir();
+        }
+    }
+}
